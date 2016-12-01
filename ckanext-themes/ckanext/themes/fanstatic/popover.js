f35e47dbf1ffae0359fbadfc355727e7dfbb2eff
holdymoldy <holdenparks@yahoo.com>
2016-12-01 10:55:43 -0800

    thumbnail popovers

--- ckanext-themes/ckanext/themes/fanstatic/popover.js
+++ ckanext-themes/ckanext/themes/fanstatic/popover.js
@@ -3,12 +3,11 @@
 ckan.module('popover', function ($, _) {
   return {
     initialize: function() {
-//      var url = '<img src="http://www.legalsportsreport.com/wp-content/uploads/2015/11/Gary-Johnson-DFS-opinion.jpg"/>';
-      var url=this.options.contents.split(",")[0].split("[u");
-      var img='<img src=' + url + '/>';
+      var img = '<img src="url"/>';
+      var url = img.replace("url",this.options.contents);
       this.el.popover({html: true,
-      title: url,
-      content: img,
+   //   title: url,
+      content: url,
       placement: 'right',
       trigger: 'hover'});
     }
